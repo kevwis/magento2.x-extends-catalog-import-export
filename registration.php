a4841ab3786624fec8a18fe351ea22fa9ba65305
Kevin <github@kevwis.com>
2017-03-27 01:20:07 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -5,6 +5,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Wsk_ImportExport',
+    'Kevwis_CatalogImportExport',
     __DIR__
 );
